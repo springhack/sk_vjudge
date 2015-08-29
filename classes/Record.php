b72cb9bc8d3f2f12d2a935ec0b55b3d74c1ca312
SpringHack <springhack@live.cn>
2015-08-29 15:03:07 +0800

    Debug HDOJ.

--- classes/Record.php
+++ classes/Record.php
@@ -17,6 +17,7 @@
 			$this->db = new MySQL();
 			$this->res = $this->db->from("Record")->where("`id` = '".$id."'")->select()->fetch_one();
 			$str = $this->res['oj']."_Record";
+			echo $str;
 			require_once(dirname(__FILE__).'/'.$str.'.php');
 			$this->record = new $str($id);
 		}
