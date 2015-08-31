58ccc0b7c97749b8788d2880870e7dd23cde878a
SpringHack <springhack@live.cn>
2015-08-31 18:52:20 +0800

    Debug HDOJ OK, add code length limit.

--- javascript/HDOJ.js
+++ javascript/HDOJ.js
@@ -1,6 +1,12 @@
-// JavaScript Document
+//Javascript Document
 
 function encodeSource()
 {
+	var str = document.getElementById("code").value;
+	if (str.length < 220)
+	{
+		alert("Code length too short!")
+		return false;
+	}
 	return true;
 }
