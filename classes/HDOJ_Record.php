89ecfeaf653f7618861144a49a06489748a1aa89
SpringHack <springhack@live.cn>
2015-08-31 17:20:55 +0800

    Debug HDOJ.

--- classes/HDOJ_Record.php
+++ classes/HDOJ_Record.php
@@ -71,10 +71,7 @@
 			**/
 			
 			$th = new HTMLParser("http://acm.hdu.edu.cn/status.php?first=".$this->res['rid']."&user=".$this->res['oj_u']);
-			echo "\n\n\n\n<pre>";
-			echo $th->getHTML();
-			echo "</pre>\n\n\n\n";
-			$th->loadHTML($th->innerHTML("<td height=22px>".$this->res['rid']."</td>", "</tr>"));
+			$th->loadHTML($th->innerHTML("<td height=22px>".$this->res['rid']."</td>", $this->res['oj_u']));
 			echo "\n\n\n\n<pre>";
 			echo $th->getHTML();
 			echo "</pre>\n\n\n\n";
