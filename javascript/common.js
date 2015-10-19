bed852c09ab668a6cf4e33eceeb431b2883e6c9c
SpringHack <SpringHack@SpringHack.local>
2015-10-19 20:39:38 +0800

    debug manager.php

--- javascript/common.js
+++ javascript/common.js
@@ -4,5 +4,5 @@ $(function () {
 				if ($(this).attr("data-type") != "rank")
 					$(this).addClass("menu");
 			});
-		$(document.body).append("<h5><a href='http://www.90its.cn/' target='_blank' style='color: #000000;'>Designed by SpringHack in Cello Studio.</a></h5>");
+		$(document.body).append("<center><h5><a href='http://www.90its.cn/' target='_blank' style='color: #000000;'>Designed by SpringHack in Cello Studio.</a></h5></center>");
 	});
